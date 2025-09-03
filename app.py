814e4b01708c8a7cdeceedebc5672761b3a35a19
dapsat100-star <dapsat100@gmail.com>
2025-09-03 10:31:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
 )
 
-name, auth_status, username = authenticator.login("Login", "sidebar")
+name, auth_status, username = authenticator.login("Login", "unrendered")
 
 if auth_status is False:
     st.error("Usuário ou senha inválidos.")
